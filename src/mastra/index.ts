090cf2a75bcde7b31a5616de53b00de5dac52fd7
reybahl <bahl.reyansh@gmail.com>
2025-10-22 22:08:54 -0400

    otel exports

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -5,5 +5,16 @@ import { copywriterAgent } from "./agents/example-copywriter-agent";
 import { editorAgent } from "./agents/example-editor-agent";
  
 export const mastra = new Mastra({
+  telemetry: {
+    serviceName: "demo-app",
+    enabled: true,
+    sampling: {
+      type: "always_on",
+    },
+    export: {
+      type: "otlp",
+      endpoint: "http://localhost:3000/api/traces",
+    },
+  },
   agents: { copywriterAgent, editorAgent, publisherAgent }
 });
