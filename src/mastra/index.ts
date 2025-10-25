5c962ae1ff29c604c4833018b6a93eb1a82ac605
reybahl <bahl.reyansh@gmail.com>
2025-10-25 10:10:14 -0400

    specify header type in telemetry

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -14,6 +14,9 @@ export const mastra = new Mastra({
     export: {
       type: "otlp",
       endpoint: "http://localhost:3000/api/traces",
+      headers: {
+        "Content-Type": "application/json",
+      },
     },
   },
   agents: { copywriterAgent, editorAgent, publisherAgent }
