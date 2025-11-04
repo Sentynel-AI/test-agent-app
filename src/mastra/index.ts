bbcf40952f9e1d19233bfe17e415e61d7f36d902
arell-git <arell.code@gmail.com>
2025-11-03 21:27:11 -0500

    changed api header name

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -16,7 +16,7 @@ export const mastra = new Mastra({
       endpoint: "http://localhost:3000/api/traces",
       headers: {
         "Content-Type": "application/json",
-        "X-API-Key": process.env.SENTYNEL_API_KEY || "",
+        "API-Key": process.env.SENTYNEL_API_KEY || "",
       },
     },
   },
