652d635b163fe3d8aabd23d1644a16bc37e549d0
reybahl <bahl.reyansh@gmail.com>
2025-11-03 21:29:29 -0500

    otel exporter

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,23 +1,31 @@
 import { Mastra } from "@mastra/core/mastra";
- 
+import { OtelExporter } from "@mastra/otel-exporter";
+
 import { publisherAgent } from "./agents/example-publisher-agent";
 import { copywriterAgent } from "./agents/example-copywriter-agent";
 import { editorAgent } from "./agents/example-editor-agent";
- 
+
 export const mastra = new Mastra({
-  telemetry: {
-    serviceName: "demo-app",
-    enabled: true,
-    sampling: {
-      type: "always_on",
-    },
-    export: {
-      type: "otlp",
-      endpoint: "http://localhost:3000/api/traces",
-      headers: {
-        "Content-Type": "application/json",
+  observability: {
+    configs: {
+      otel: {
+        serviceName: "demo-app",
+        exporters: [
+          new OtelExporter({
+            provider: {
+              custom: {
+                endpoint: "http://localhost:3000/api/traces",
+                protocol: "http/json",
+                headers: {
+                  "Content-Type": "application/json",
+                  "X-API-Key": "default-api-key",
+                },
+              },
+            },
+          }),
+        ],
       },
     },
   },
-  agents: { copywriterAgent, editorAgent, publisherAgent }
+  agents: { copywriterAgent, editorAgent, publisherAgent },
 });
